af7efa0546d30802160e31c182be526ce5311684
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2023-01-15 10:47:30 +0800

    Add files via upload

---
+++ app.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# run myapp
+# notice: already "COPY app.json /etc/uwsgi/app.json" at Dockerfile
+/usr/bin/uwsgi -config=/etc/uwsgi/app.json &
+
+#run nginx
+# CMD ["nginx", "-g", "daemon off;"]
+nginx -g 'daemon off;'
+# /usr/local/nginx/sbin/nginx -g 'daemon off;'
+
